317e095453f60a1bdf83ea5d5c4ee00d2a4f62c9
nevoniuk <nevoniuk@purdue.edu>
2022-08-03 17:42:29 -0700

    debugging

--- services/calc/calc.go
+++ services/calc/calc.go
@@ -46,6 +46,10 @@ func NewCalc(ctx context.Context, psc power.Client, dbc storage.Client, fc facil
 // HistoricalCarbonEmissions will output the CO2 intensity, Power Meter, and resulting CO2 emission reports
 func (s *calcSvc) HistoricalCarbonEmissions(ctx context.Context, req *gencalc.RequestPayload) (*gencalc.AllReports, error) {
 	fmt.Println("IN METHOD")
+	if req == nil {
+		fmt.Println("req is null")
+	}
+	fmt.Println(req.OrgID)
 	log.Info(ctx, log.KV{K: "orgID", V: req.OrgID}, 
 		log.KV{K: "facilityID", V: req.FacilityID},
 		log.KV{K: "locationID", V: req.LocationID},
