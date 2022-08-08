d67a2a2de1bb7654c973f086c54b173aab088f60
nevoniuk <nevoniuk@purdue.edu>
2022-08-08 13:23:12 -0700

    fixed error in client.go

--- services/calc/error.go
+++ services/calc/error.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"goa.design/clue/log"
-	goa "goa.design/goa/v3/pkg"
 	"github.com/crossnokaye/carbon/services/calc/clients/storage"
 	"github.com/crossnokaye/carbon/services/calc/clients/power"
 	"github.com/crossnokaye/carbon/services/calc/clients/facilityconfig"
@@ -21,26 +20,26 @@ func mapAndLogErrorf(ctx context.Context, format string, a ...interface{}) error
 // It logs the error using the context if it does not map to a design error
 // (i.e. is unexpected).
 func mapAndLogError(ctx context.Context, err error) error {
-	var gerr *goa.ServiceError
 	var carbonreportsNotFound storage.ErrReportsNotFound
 	var fNotFound facilityconfig.ErrFacilityNotFound
 	var lNotFound facilityconfig.ErrLocationNotFound
 	var powerreportsNotFound power.ErrPowerReportsNotFound
 	if errors.As(err, &carbonreportsNotFound) {
-		gerr = gencalc.MakeReportsNotFound(carbonreportsNotFound)
+		log.Error(ctx, err)
+		return gencalc.MakeReportsNotFound(carbonreportsNotFound)
 	}
 	if errors.As(err, &fNotFound) {
-		gerr = gencalc.MakeFacilityNotFound(fNotFound)
+		log.Error(ctx, err)
+		return gencalc.MakeFacilityNotFound(fNotFound)
 	}
 	if errors.As(err, &lNotFound) {
-		gerr = gencalc.MakeFacilityNotFound(lNotFound)
+		log.Error(ctx, err)
+		return gencalc.MakeFacilityNotFound(lNotFound)
 	}
 	if errors.As(err, &powerreportsNotFound) {
-		gerr = gencalc.MakeReportsNotFound(powerreportsNotFound)
-	} else {
-		log.Errorf(ctx, err, "Error not found: %w", err)
-		gerr = gencalc.MakeReportsNotFound(err)
+		log.Error(ctx, err)
+		return gencalc.MakeReportsNotFound(powerreportsNotFound)
 	}
-	log.Error(ctx, gerr)
+	log.Error(ctx, err)
 	return err
 }
